08a6bbf1cec59ef30af28627876e851322f5d2ab
Philip Lewis <pcl@pclewis.com>
2017-01-24 00:38:54 -0500

    nix: xwidgets in emacs, user pkgs in config.nix

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -1,1 +1,55 @@
-{ allowUnfree = true; }
+{
+  allowUnfree = true;
+
+  packageOverrides = pkgs: {
+
+    ## Enable XWidgets support in emacs, because why not
+    #
+    emacs = pkgs.emacs.override { withGTK2=false; withGTK3=true; withXwidgets=true; };
+
+    ## Packages installed in user environment
+    #
+    # update with:
+    #   nix-env -u -A --always nixos.pclenv
+    #
+    # sort with:
+    #   v i [ SPC x l s
+    #
+    pclenv = with pkgs; buildEnv {
+      name = "pclenv";
+      paths = [
+        acpi
+        davmail
+        docker
+        dos2unix
+        dtrx
+        dunst
+        dzen2
+        emacs
+        gnupg
+        htop
+        imagemagick
+        jq
+        lastpass-cli
+        leiningen
+        libnotify
+        lsof
+        mpv
+        mu
+        ncmpcpp
+        nmap
+        offlineimap
+        p7zip
+        pwgen
+        qutebrowser
+        rdesktop
+        scrot
+        tcpdump
+        traceroute
+        unclutter
+        xclip
+      ];
+    };
+
+  };
+}
