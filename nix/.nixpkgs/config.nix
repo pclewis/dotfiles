8eb2513ee069b54fb5c0edb11fe322944fc39775
Philip Lewis <pcl@pclewis.com>
2017-01-24 22:49:58 -0500

    nix: qutebrowser patches

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -1,21 +1,48 @@
 {
+
   allowUnfree = true;
 
-  packageOverrides = pkgs: {
+  packageOverrides = originalPackages: let newPackages = originalPackages.pkgs; in {
 
-    ## Enable XWidgets support in emacs, because why not
+    #-------------#
+    # qutebrowser #
+    #-------------#
     #
-    emacs = pkgs.emacs.override { withGTK2=false; withGTK3=true; withXwidgets=true; };
+    # override changes arguments to derivation (ex options and dependencies)
+    # overrideAttrs changes the attrs of the actual derivation (ex version, build steps)
+    #
+    qutebrowser = (originalPackages.qutebrowser.override {
+      ##
+      # fix qtwebkit-plugins so notifications and spellcheck work
+      ##
+      qtwebkit-plugins = originalPackages.qt5.qtwebkit-plugins.overrideAttrs(oldAttrs: {
+        postPatch = oldAttrs.postPatch + ''
+          sed -i "s,plugin.h,plugin.h ${newPackages.qt5.qtwebkit.dev}/include/QtWebKit/qwebkitplatformplugin.h," src/src.pro  
+        '';
+      });
+    }).overrideAttrs(oldAttrs: rec {
+      ##
+      # 0.9.0 -> 0.9.1
+      ##
+      version = assert oldAttrs.version == "0.9.0"; "0.9.1";
+      name = "qutebrowser-${version}";
+      src = originalPackages.fetchurl {
+        url = "https://github.com/The-Compiler/qutebrowser/releases/download/v${version}/${name}.tar.gz";
+        sha256 = "0pf91nc0xcykahc3x7ww525c9czm8zpg80nxl8n2mrzc4ilgvass";
+      };
+    });
 
-    ## Packages installed in user environment
+    #----------------------------------------# 
+    # Packages installed in user environment #
+    #----------------------------------------#
     #
     # update with:
-    #   nix-env -u -A --always nixos.pclenv
+    #   nix-env -uA --always nixos.pclenv
     #
     # sort with:
     #   v i [ SPC x l s
     #
-    pclenv = with pkgs; buildEnv {
+    pclenv = with newPackages; buildEnv {
       name = "pclenv";
       paths = [
         acpi
