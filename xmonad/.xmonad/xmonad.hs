06305ea72dda6149aa00d468a4e59a9989acd056
Philip Lewis <pcl@pclewis.com>
2017-01-31 12:49:46 -0500

    xmonad: ok 4px is too many

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -57,7 +57,7 @@ myFocusFollowsMouse = True
 
 -- Width of the window border in pixels.
 --
-myBorderWidth   = 4
+myBorderWidth   = 1
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
