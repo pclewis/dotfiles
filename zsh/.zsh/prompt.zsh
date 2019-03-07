db37342f4f8f253b697445c1e7690c3d4fd549fa
Philip Lewis <pcl@pclewis.com>
2019-03-07 13:29:08 -0500

    zsh: improve prompt
    - don't clobber command with timestamp
    - fix reprinted prompt disappearing sometimes
    - explain what the heck is even going on, because honestly

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,3 +1,7 @@
+# %K/%k - start/stop background color
+# %F/%f - start/stop foreground color
+# %E    - clear to end of line - doesn't move cursor (ANSI Erase Line, \e[K)
+
 ## _prompt_pwd():
 #
 #  Fancy colorized prompt PWD.
@@ -16,7 +20,7 @@ function _prompt_pwd() {
     d=$PWD
     s=$d:t
     d=$d:h
-    while true; do 
+    while true; do
         if [[ "$d" == "$HOME" ]]; then
             s="~/"$s
             break;
@@ -52,7 +56,7 @@ function _prompt_pwd() {
 }
 
 ## _colorize(str):
-# 
+#
 #  Return str with a random color based on its content.
 #
 function _colorize() {
@@ -65,15 +69,73 @@ function _colorize() {
 ## _old_prompt(input,cmd,fullcmd)
 #
 #  Make prompts look different when they're history
+#  - Set background color of whole line
+#  - Remove syntax highlighting from entered command
+#  - Add timestamp on right edge of screen
 #
 function _old_prompt() {
+    # print: -n Do not add newline
+    #        -r Do not process escape sequences
+    #        -P Perform prompt expansion
+
+    # We are currently on the line below the first line of the input command.
+
+    # Turn off colors and erase anything on this line (ex RPROMPT)
     print -nP '%k%f%E'
+
+    # Move back up to line with prompt
     tput cuu1
-    print -nP '%K{237}%E\r'
-    COL=$(($(tput cols) - 22))
-    print -nP "%K{237}$PROMPT%K{237}"
-    print -nR $1
-    print -nP "\e[${COL}G%F{240}" $(date '+[%Y-%m-%d %H:%M:%S]') '%k%f'
+
+    # Set background color of entire line
+    print -nP '%K{237}%E'
+
+    # Re-print prompt
+    print -nP "$PROMPT"
+
+    # Make sure we still have our background color
+    print -nP '%K{237}'
+
+    # Re-print command
+    print -nr "$1"
+
+    # Figure out where we are
+    print -n "\e[6n" # ESC [ Ps n (DSR)
+
+    # Terminal response is ESC [ ROW ; COL R
+
+    # read: -s  Turn off terminal echo
+    #       -dR Input is terminated by R
+    IFS=';' read -sdR CUR_ROW CUR_COL
+
+    # Sometimes terminal response gets echoed anyway, clear it out
+    tput hpa $((CUR_COL - 1))
+    print -nP '%E'
+
+    # Figure out where we want to print tag
+    TAG="$(date '+[%Y-%m-%d %H:%M:%S]')"
+    TAG_LEN=${#TAG}
+    DST_COL=$(($(tput cols) - TAG_LEN))
+
+    # If no room, add an extra line
+    if [[ $DST_COL -lt $CUR_COL ]]; then
+        print -nP '%E\n%E\n%K%E'
+        tput cuu1
+        print -nP '%K{237}'
+    fi
+
+    # Go to tag column
+    tput hpa $DST_COL # Move cursor to column, ESC [ Ps G
+
+    # Print tag
+    print -nP '%F{240}'
+    print -nr "$TAG"
+    print -nP '%k%f'
+
+    # Next character will appear at the beginning of the next row, but we're
+    # actually still on the same line, so ex cursor positioning will not work
+    # as expected, and window resizes might do weird things. So explicitly move
+    # down to the next row, which is visually a no-op.
+    tput cud1
 }
 
 add-zsh-hook preexec _old_prompt
